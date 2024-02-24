764902f65d06d6f0daa92ad4de2ac10175e9e23a
imertetsu <imertpro@gmail.com>
2024-02-23 20:28:43 -0500

    rest api added

--- python/filesRW.py
+++ python/filesRW.py
@@ -1,10 +1,10 @@
 #here we play with r and w
-file1 = open('D:\\Imer\\ProgrammingPractice\\Python\\Spark\\sparkIntroduction\\python\\hola.txt','w')
+file1 = open('D:\\Imer\\ProgrammingPractice\\Python\\Spark\\sparkIntroduction\\python\\hola.txt','r')
 
 print('this is the route '+file1.name)
 
-print('esta es la primera fila '+file1.readline())
-print(file1.read())
+print('esta es la 4ta fila '+file1.readline(8))
+#print(file1.read())
 
 #file1.write('this is new written')
 file1.close()
